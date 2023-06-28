bdfe77baeff49c1159e4e57367ccc705d7c7d7bd
Pierpaolo Calanna <p.calanna@gmail.com>
2023-06-28 20:41:07 +0200

    bugfix

--- src/components/report/ReportTemplate.vue
+++ src/components/report/ReportTemplate.vue
@@ -27,27 +27,27 @@ const date = new Date().toISOString().slice(0, 10)
 // init flags
 const flags = Array.from({ length: 21 }, () => false)
 // check flags
-flags[0] = items[0] > 0
-flags[1] = items[1] > 0
-flags[2] = items[2] > 0
-flags[3] = items[3] > 0
-flags[4] = items[4] > 0
-flags[5] = items[5] > 0
-flags[6] = items[6] > 0
-flags[7] = items[7] > 0
-flags[8] = items[8] > 0
-flags[9] = items[9] > 0
-flags[10] = items[10] > 3
-flags[11] = items[11] > 2
-flags[12] = items[12] > 3
-flags[13] = items[13] > 0
-flags[14] = items[14] > 0
-flags[15] = items[15] > 0
-flags[16] = items[16] > 0
-flags[17] = items[17] > 0
-flags[18] = items[18] > 0
-flags[19] = items[19] > 0
-flags[20] = items[20] < 7
+flags[0] = items[0] > 0 || items[0] == -1
+flags[1] = items[1] > 0 || items[1] == -1
+flags[2] = items[2] > 0 || items[2] == -1
+flags[3] = items[3] > 0 || items[3] == -1
+flags[4] = items[4] > 0 || items[4] == -1
+flags[5] = items[5] > 0 || items[5] == -1
+flags[6] = items[6] > 0 || items[6] == -1
+flags[7] = items[7] > 0 || items[7] == -1
+flags[8] = items[8] > 0 || items[8] == -1
+flags[9] = items[9] > 0 || items[9] == -1
+flags[10] = items[10] > 3 || items[10] == -1
+flags[11] = items[11] > 2 || items[11] == -1
+flags[12] = items[12] > 3 || items[12] == -1
+flags[13] = items[13] > 0 || items[13] == -1
+flags[14] = items[14] > 0 || items[14] == -1
+flags[15] = items[15] > 0 || items[15] == -1
+flags[16] = items[16] > 0 || items[16] == -1
+flags[17] = items[17] > 0 || items[17] == -1
+flags[18] = items[18] > 0 || items[18] == -1
+flags[19] = items[19] > 0 || items[19] == -1
+flags[20] = items[20] < 7 || items[20] == -1
 </script>
 
 <template>
