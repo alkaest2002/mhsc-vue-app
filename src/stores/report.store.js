137b514fd344f643b85685e8f090ec538222a52d
Pierpaolo Calanna <p.calanna@gmail.com>
2023-07-05 17:57:03 +0200

    added validations for report settings

--- src/stores/report.store.js
+++ src/stores/report.store.js
@@ -1,7 +1,33 @@
 import { defineStore } from 'pinia'
 
+const baseSettings = () => ({
+  typeOfReport: 'screen',
+  highlightPositiveItems: true
+})
+
+const getReportSettings = (settings) => {
+  // try to validate settings
+  try {
+    // apply rules
+    const settingsAreOk = [
+      Object.keys(settings).sort().join() ==
+        ['typeOfReport', 'highlightPositiveItems'].sort().join(),
+      ['screen', 'download', 'print'].includes(settings.typeOfReport),
+      settings.highlightPositiveItems === true || settings.highlightPositiveItems == false
+    ].every(Boolean)
+    // if validations are passed
+    if (settingsAreOk) return { ...settings }
+    // return base settings
+    return baseSettings()
+    // on error
+  } catch (err) {
+    // return base settings
+    return baseSettings()
+  }
+}
+
 export const useReportStore = defineStore('reportStore', {
   state: () => {
-    return { ...window.reportSettings }
+    return getReportSettings(window.reportSettings)
   }
 })
