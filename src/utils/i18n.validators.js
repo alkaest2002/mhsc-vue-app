7313f31ba2da4b2b39df503e4a38f4acf9ba268d
Pierpaolo Calanna <p.calanna@gmail.com>
2023-07-06 18:39:44 +0200

    simplified code

--- src/utils/i18n.validators.js
+++ src/utils/i18n.validators.js
@@ -2,15 +2,12 @@ import * as validators from '@vuelidate/validators'
 import { i18n } from '@/i18n'
 
 const { createI18nMessage } = validators
-const { withMessage, regex } = validators.helpers
+const { regex} = validators.helpers
 const withI18nMessage = createI18nMessage({ t: i18n.global.t.bind(i18n) })
 
 export const required = withI18nMessage(validators.required)
 export const alpha = withI18nMessage(validators.alpha)
 export const maxLength = withI18nMessage(validators.maxLength, { withArguments: true })
-export const extendedAlpha = withI18nMessage(
-  withMessage('validations.extendedAlpha', regex(/^[a-zA-Z'()\- ]*$/))
-)
-export const isDate = withI18nMessage(
-  withMessage('validations.isDate', regex(/^(\d{2}){2}-\d{2}-(\d{2})$/gm))
-)
+export const extendedAlpha = withI18nMessage(regex(/^[a-zA-Z'()\- ]*$/))
+export const isDate = withI18nMessage(regex(/^(\d{2}){2}-\d{2}-(\d{2})$/gm))
+
