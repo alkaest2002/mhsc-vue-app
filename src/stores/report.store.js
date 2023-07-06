76aeedf1f8143c72928af6093cfacc541808750c
Pierpaolo Calanna <p.calanna@gmail.com>
2023-07-06 19:16:48 +0200

    refactored code

--- src/stores/report.store.js
+++ src/stores/report.store.js
@@ -1,29 +1,28 @@
+import { getCurrentInstance, } from 'vue'
 import { defineStore } from 'pinia'
+import { useVuelidate } from '@vuelidate/core'
+import { isBoolean, enumeration, required } from '@/utils/i18n.validators'
 
-const baseSettings = () => ({
+// base degault settings
+const defaultSettings = () => ({
   typeOfReport: 'screen',
   highlightPositiveItems: true
 })
 
 const getReportSettings = (settings) => {
-  // try to validate settings
-  try {
-    // apply rules
-    const settingsAreOk = [
-      Object.keys(settings).sort().join() ==
-        ['typeOfReport', 'highlightPositiveItems'].sort().join(),
-      ['screen', 'download', 'print'].includes(settings.typeOfReport),
-      settings.highlightPositiveItems === true || settings.highlightPositiveItems == false
-    ].every(Boolean)
-    // if validations are passed
-    if (settingsAreOk) return { ...settings }
-    // return base settings
-    return baseSettings()
-    // on error
-  } catch (err) {
-    // return base settings
-    return baseSettings()
+  // define validation rules
+  const rules = {
+    typeOfReport: { required, enumeration: enumeration(['screen', 'download', 'print']) },
+    highlightPositiveItems: { isBoolean },
   }
+  // init vuelidate instance
+  const v = useVuelidate(rules, settings, { getCurrentInstance: getCurrentInstance() })
+  // validate settings
+  v.value.$touch()
+  // if validation is ok
+  if (v.value.$errors.length == 0) return { ...settings }
+  // otherwise return base settings
+  return defaultSettings()
 }
 
 export const useReportStore = defineStore('reportStore', {
--- src/utils/i18n.validators.js
+++ src/utils/i18n.validators.js
@@ -2,7 +2,7 @@ import * as validators from '@vuelidate/validators'
 import { i18n } from '@/i18n'
 
 const { createI18nMessage } = validators
-const { regex} = validators.helpers
+const { regex } = validators.helpers
 const withI18nMessage = createI18nMessage({ t: i18n.global.t.bind(i18n) })
 
 export const required = withI18nMessage(validators.required)
@@ -10,4 +10,5 @@ export const alpha = withI18nMessage(validators.alpha)
 export const maxLength = withI18nMessage(validators.maxLength, { withArguments: true })
 export const extendedAlpha = withI18nMessage(regex(/^[a-zA-Z'()\- ]*$/))
 export const isDate = withI18nMessage(regex(/^(\d{2}){2}-\d{2}-(\d{2})$/gm))
-
+export const isBoolean = withI18nMessage((value) => value === true || value === false)
+export const enumeration = withI18nMessage((list) => (value) => list.includes(value) , { withArguments: true })
